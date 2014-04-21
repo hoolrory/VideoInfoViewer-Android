a2c24fc9dfa6065160719418c9b5ca7c17d5eb40
Rory Hool <hoolrory@gmail.com>
2014-04-20 22:45:48 -0400

    Comment out unused fields

--- VideoInfoViewer/src/com/roryhool/videoinfoviewer/search/SearchItem.java
+++ VideoInfoViewer/src/com/roryhool/videoinfoviewer/search/SearchItem.java
@@ -19,7 +19,7 @@ package com.roryhool.videoinfoviewer.search;
 import com.coremedia.iso.boxes.Box;
 
 public class SearchItem {
-
+   /*
    private enum SearchItemType {
       BOX, FIELD
    }
@@ -28,12 +28,12 @@ public class SearchItem {
 
    private String mTitle;
    private String mDescription;
-
+    */
    public static void CreateFromBox( Box box ) {
 
    }
 
    public static void CreateFromField( String field, String value, Box box ) {
-
+      
    }
 }
