57cb2386e4ebab2c5d3beae642186dc32d8720ea
Rory Hool <hoolrory@gmail.com>
2014-10-07 12:32:11 -0400

    Migrate to Gradle / Android Studio. Had to fix one 9 patch image

---
+++ app/build/generated/source/buildConfig/debug/com/roryhool/videoinfoviewer/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.roryhool.videoinfoviewer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.roryhool.videoinfoviewer";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 110;
+  public static final String VERSION_NAME = "";
+  /**
+   * @deprecated Use {@link #APPLICATION_ID}
+   */
+  @Deprecated
+  public static final String PACKAGE_NAME = "com.roryhool.videoinfoviewer";
+}
