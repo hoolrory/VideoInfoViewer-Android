087769817a2b013b72e2d253d01db282eb946efa
Rory Hool <hoolrory@gmail.com>
2015-08-13 19:52:59 -0400

    Android Studio stuff

--- app/build/generated/source/buildConfig/debug/com/roryhool/videoinfoviewer/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/roryhool/videoinfoviewer/BuildConfig.java
@@ -8,11 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.roryhool.videoinfoviewer";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 110;
+  public static final int VERSION_CODE = 111;
   public static final String VERSION_NAME = "";
-  /**
-   * @deprecated Use {@link #APPLICATION_ID}
-   */
-  @Deprecated
-  public static final String PACKAGE_NAME = "com.roryhool.videoinfoviewer";
 }
